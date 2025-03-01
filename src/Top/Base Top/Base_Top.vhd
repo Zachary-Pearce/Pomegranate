33b6cb5985bc4706c2f4e54fadc0870244c901fc
Zachary Pearce <zachary.pearce25@gmail.com>
2025-03-01 17:30:41 +0000

    updated libraries

--- src/Top/Base Top/Base_Top.vhd
+++ src/Top/Base Top/Base_Top.vhd
@@ -21,7 +21,8 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use WORK.pomegranate_conf.ALL;
+use WORK.pomegranate_inst_conf.ALL;
+use WORK.pomegranate_memory_map_conf.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
