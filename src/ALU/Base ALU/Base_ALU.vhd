5a996540aa401763478edc6f4e7ba692c88de78c
Zachary Pearce <zachary.pearce25@gmail.com>
2025-03-01 16:15:37 +0000

    solved errors and standardised

--- src/ALU/Base ALU/Base_ALU.vhd
+++ src/ALU/Base ALU/Base_ALU.vhd
@@ -1,25 +1,24 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
-use WORK.pomegranate_conf.all;
 
 entity ALU is
     generic (
-        n: natural := 8 --data width
+        WORD_WIDTH: natural := 8 --data width
     );
     Port (
         ALU_EN, ALU_IMM: in std_logic;
         ALU_OP: in std_logic_vector(2 downto 0);
-        Rs, Rt, Immediate: in std_logic_vector(n-1 downto 0);
-        result_out: out std_logic_vector(n-1 downto 0);
+        Rs, Rt, Immediate: in std_logic_vector(WORD_WIDTH-1 downto 0);
+        result_out: out std_logic_vector(WORD_WIDTH-1 downto 0);
         C_flag, Z_flag, N_flag, P_flag: out std_logic
     );
 end entity ALU;
 
 architecture ALU_RTL of ALU is
 --signals
-signal result: std_logic_vector(n downto 0);
-signal source, target: std_logic_vector(n-1 downto 0);
+signal result: std_logic_vector(WORD_WIDTH downto 0);
+signal source, target: std_logic_vector(WORD_WIDTH-1 downto 0);
 begin
     --COMBINATIONAL PART
     --get the source and target
@@ -29,19 +28,19 @@ begin
     
     --select ALU operation
     with ALU_OP select
-        result  <= std_logic_vector(unsigned('0' & source) + unsigned('0' & target)) when ALU_OP = "000",
-                <= '0' & std_logic_vector(unsigned(source) - unsigned(target)) when ALU_OP = "001",
-                <= '0' & source and '0' & target when ALU_OP = "010",
-                <= '0' & source or '0' & target when ALU_OP = "011",
-                <= '0' & source xor '0' & target when ALU_OP "100",
-                <= '0' & (not source) when ALU_OP = "101",
-                <= std_logic_vector(shift_left(unsigned('0' & source), 1)) when ALU_OP = "110",
-                <= std_logic_vector(shift_right(unsigned('0' & source), 1)) when ALU_OP = "111";
+        result  <= std_logic_vector(unsigned('0' & source) + unsigned('0' & target)) when "000",
+                <= '0' & std_logic_vector(unsigned(source) - unsigned(target)) when "001",
+                <= '0' & source and '0' & target when "010",
+                <= '0' & source or '0' & target when "011",
+                <= '0' & source xor '0' & target when "100",
+                <= '0' & (not source) when "101",
+                <= std_logic_vector(shift_left(unsigned('0' & source), 1)) when "110",
+                <= std_logic_vector(shift_right(unsigned('0' & source), 1)) when "111";
     
     --OUTPUT PART
-    result_out <= result(n-1 downto 0) when ALU_EN = '1' else (others => 'Z');
-    Z_flag <= '1' when result(n-1 downto 0) = zeros else '0';
-    N_flag <= result(n-1); --2's complement, if the MSB is set then the result is negative
-    P_flag <= not result(n-1); --2's complement, if the MSB is cleared then the result is positive
-    C_flag <= result(n); --the last bit of the result signal is the carry
+    result_out <= result(WORD_WIDTH-1 downto 0) when ALU_EN = '1' else (others => 'Z');
+    Z_flag <= '1' when unsigned(result(WORD_WIDTH-1 downto 0)) = 0 else '0';
+    N_flag <= result(WORD_WIDTH-1); --2's complement, if the MSB is set then the result is negative
+    P_flag <= not result(WORD_WIDTH-1); --2's complement, if the MSB is cleared then the result is positive
+    C_flag <= result(WORD_WIDTH); --the last bit of the result signal is the carry
 end ALU_RTL;
